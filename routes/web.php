5fe836ab5e7b0b5012fd66edc9a5245b2a71f11a
unknown <ghinanurhalidah23@mhs.politala.ac.id>
2024-10-13 07:35:25 +0800

    mengubah coding

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home');
-});
+
+//route resource for products
+Route::resource('/products', \App\Http\Controllers\ProductController::class);
+
