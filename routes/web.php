9e0336556f95d514fe6cfa3d17c9bbefc07e189b
unknown <ghinanurhalidah23@mhs.politala.ac.id>
2024-10-13 15:47:34 +0800

    menyelesaikan halaman home

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
 
 //route resource for products
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
 
+Route::get('/Login', function () {
+    return view('Login');
+});
